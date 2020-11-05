88f82f34742687cda8b3cd65e5135ea725d79d86
Ovi Rahman <m.ovirahman@outlook.com>
2020-11-05 17:53:23 +0600

    ..

--- R/bd_plot.R
+++ R/bd_plot.R
@@ -2,15 +2,43 @@
 #'
 #' uses ggplot2
 #'
-#' @import ggplot2
+#' @import tmap
 #' @export
 
 bd_plot <-
-  function(level = "country"){
+  function(level = "country", type = "interactive"){
 
-    if(level %in% c("country", "division", "district", "upazilla")){
-      ggplot2::ggplot(get(paste0("map_", level))) + ggplot2::geom_sf() + ggplot2::theme_void()
-    }else {
-      stop("incorrect level name")
-    }
+    switch (type,
+      static = {tmap_mode("plot")},
+      interactive = {tmap_mode("view")},
+      stop('incorrect plotting mode. posible options: "static", "interactive"')
+    )
+
+    switch (level,
+      country = {
+        tm_shape(map_country) + tm_polygons(col = "Country")
+      },
+      division = {
+        tm_shape(map_division) + tm_polygons(col = "Division")
+      },
+      district = {
+        tm_shape(map_district) + tm_polygons(col = "Division")
+      },
+      upazilla = {
+        tm_shape(map_upazilla) + tm_polygons(col = "Division")
+        },
+      stop('incorrect level name. possible options: "country", "division", "district", "upazilla"')
+    )
   }
+
+
+
+# bd_plot <-
+#   function(level = "country"){
+#
+#     if(level %in% c("country", "division", "district", "upazilla")){
+#       ggplot2::ggplot(get(paste0("map_", level))) + ggplot2::geom_sf() + ggplot2::theme_void()
+#     }else {
+#       stop("incorrect level name")
+#     }
+#   }
